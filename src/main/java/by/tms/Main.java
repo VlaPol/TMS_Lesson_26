caedb4ae791343c809f6e2cb5dfddb9baca6b926
pva <tango2002@gmail.com>
2023-04-15 18:59:31 +0300

    add logger

--- src/main/java/by/tms/Main.java
+++ src/main/java/by/tms/Main.java
@@ -5,11 +5,17 @@ import by.tms.repository.TVShowRepository;
 import by.tms.repository.TVShowRepositoryImpl;
 import by.tms.service.ShowService;
 import by.tms.service.ShowServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class Main {
+
+    public static final Logger LOG = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+
+        LOG.info("Приложение запущено");
 
         TVShowRepository tvShowRepository = new TVShowRepositoryImpl();
         ShowService tvShowService = new ShowServiceImpl(tvShowRepository);
